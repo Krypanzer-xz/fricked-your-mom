bb75640318706ec526d59f816ca4607f9cef4720
Krypanzer <78031172+Krypanzer@users.noreply.github.com>
2021-01-27 01:59:10 +0800

    some random madlib made in 2 AM

---
+++ madlib.py
@@ -0,0 +1,17 @@
+# # goal is to create a madlib
+
+print ("Python is pretty ______ that I wish to _______. seriously, you can never just go one day without feeling like ___________, \
+I really wish this could be _____ so it would just make work a lot easier. oh, and subscribe to _________. they're \
+great.")
+
+adj = input ("Enter a Adjective: ")
+verb1 = input ("Enter a Verb: ")
+infamous_person = input ("Enter international dickheads here: ")
+verb2 = input ("Enter another Verb: ")
+youtube_personality = input ("Enter your favorite youtuber: ")
+
+madlib = f"Python is pretty {adj} I wish to {verb1} seriously, you can never just go one day without feeling like {infamous_person}, \
+I really wish this could be {verb2} so it would just make work a lot easier. oh, and subscribe to {youtube_personality}. they're \
+great."
+
+print (madlib)
